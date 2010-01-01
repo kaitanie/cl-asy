ca8b550727f4c043006b1f1192f1df9bf3cd6e89
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-01-01 17:11:50 +0100

    Added asy-generator for datapoint plotting

--- datapoints.lisp
+++ datapoints.lisp
@@ -14,12 +14,19 @@
     :initarg :dy
     :initform 0.0)))
 
+(defmethod datapoint2d-x ((point datapoint2d))
+  (with-slots (x) point
+    x))
+
+(defmethod datapoint2d-y ((point datapoint2d))
+  (with-slots (y) point
+    y))
+
 (defmethod datapoint2d-print ((point datapoint2d))
   (with-slots (x y dx dy) point
     (format t (concatenate 'string
 			   "(x = " (write-to-string x) "+-" (write-to-string dx) ","
 			   " y = " (write-to-string y) "+-" (write-to-string dy) ")~%"))))
-
 
 (defclass datagraph2d ()
   ((title
@@ -39,3 +46,17 @@
   (with-slots (points) graph2d
     (dolist (point points)
       (funcall printer point))))
+
+(defmacro generate-asy-graphing-function (title xlabel ylabel list-of-xvalues list-of-yvalues)
+  `(concatenate 'string "plot_" ,title "() {~%"
+		"x = {" (array-as-string ,list-of-xvalues) "};~%"
+		"y = {" (array-as-string ,list-of-yvalues) "};~%"
+		"points(x, y);~%"
+		"}~%"))
+
+(defmethod datagraph2d-plot ((graph2d datagraph2d))
+  (with-slots (title xlabel ylabel points) graph2d
+    (let ((xvalues (mapcar #'datapoint2d-x points))
+	  (yvalues (mapcar #'datapoint2d-y points)))
+      (concatenate 'string
+		   (generate-asy-graphing-function title xlabel ylabel xvalues yvalues)))))
