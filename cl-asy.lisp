0dea8431ef89d79bc98d592d81f22f6dc718cff6
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2010-01-07 22:19:45 +0100

    Export datapoint2d and datagraph2d types and methods

--- cl-asy.lisp
+++ cl-asy.lisp
@@ -5,4 +5,9 @@
 	   :histo1d-print
 	   :histo1d-plot
 	   :create-linear-binning
+	   :datapoint2d
+	   :datapoint2d-x
+	   :datapoint2d-y
+	   :datagraph2d
+	   :datagraph2d-plot
 	   :datapoint2d))
